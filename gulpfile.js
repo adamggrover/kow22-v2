ca4d0b71191232c6c0276435f9dbaf55c870dd8e
Adam Grover <your_email@example.com>
2022-09-14 14:32:36 +0100

    Create initial files

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const { src, dest, watch, series } = require('gulp');
+const postcss = require('gulp-postcss');
+// const sharpResponsive = require("gulp-sharp-responsive");
+
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const autoprefixer = require('autoprefixer');
+const browsersync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+
+// Sass Task
+function scssTask(){
+    return src('src/scss/styles.scss', {sourcemaps : true})
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('dist/css/styles.css', {sourcemaps: '.'}));
+}
+
+// Js Task
+function jsTask(){
+    return src('src/js/**/*.js', {sourcemaps : true})
+    .pipe(terser())
+    .pipe(dest('dist/main.js', {sourcemaps: '.'}));
+
+}
+
+
+
+// Brosersync Tasks
+function browsersyncServe(cb){
+    browsersync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    cb();
+
+}
+
+function browsersyncReload(cb){
+    browsersync.reload();
+    cb();
+}
+
+//Sharp Responsive Task
+
+//const img = () => src("src/assets/images/**/*.{jpg,png}")
+/*  .pipe(sharpResponsive({
+    formats: [
+      { width: 440, jpegOptions: { quality: 70, progressive: true }, rename: { suffix: "-sm" } },
+      { width: 740, jpegOptions: { quality: 70, progressive: true }, rename: { suffix: "-md" } },
+      { width: 1024, jpegOptions: { quality: 70, progressive: true }, rename: { suffix: "-lg" } },
+      { width: 1800, jpegOptions: { quality: 70, progressive: true }, rename: { suffix: "-xl" } },
+    ]
+  }))
+  .pipe(dest("dist/images"));
+ */ 
+
+// Watch Task
+function watchTask(){
+    //watch('*html', browsersyncReload);
+    watch(['*html, src/scss/**/*.scss', 'src/js/**/*.js']), series(scssTask, jsTask 
+        //browsersyncReload
+        );
+
+}
+
+// Default Gulp Task
+exports.default = series(
+    scssTask,
+    jsTask,
+   // img,
+    //browsersyncServe,    
+    watchTask
+)
---
+++ src/js/main.js
@@ -0,0 +1,202 @@
+
+
+// When the user scrolls down 50px from the top of the document, change the header's background color
+
+
+function headerBackground() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.backgroundColor = "#231F20";
+  } else {
+    document.getElementById("header").style.backgroundColor = "transparent";
+  }
+}
+
+//function to change logo size on scroll
+
+function logoChange(){
+    var logo = $(".logo1"); $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    
+
+    
+    if (scroll >= 50) {
+        if(!logo.hasClass("logo2")) {
+          logo.hide();
+          logo.removeClass('logo1').addClass("logo2").fadeIn( "slow");
+        }
+      } else {
+        if(!logo.hasClass("logo1")) {
+          logo.hide();
+          logo.removeClass("logo2").addClass('logo1').fadeIn( "slow");
+        }
+      }
+    });
+};
+
+// Header change on scroll
+
+if ( document.URL.includes("index") ) {
+  window.onscroll = function() {
+      headerBackground();
+      logoChange();
+      };
+}
+
+
+//Search Bar
+
+
+
+  /* Set the height of the side navigation to 250px and the left margin of the page content to 250px */
+function openSearch() {
+    document.getElementById("header-search-container").style.height = "3rem";
+    document.getElementById("header-container").style.marginTop = "3rem";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeSearch() {
+    document.getElementById("header-search-container").style.height = "0";
+    document.getElementById("header-container").style.marginTop = "0";
+  }
+
+
+// Sidenav
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("sidebar-nav").style.width = "26rem";
+    document.getElementById("main").style.marginLeft = "26rem";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.8)";
+    window.onscroll = () => { window.scroll(0, 0); };
+    
+
+}
+
+ 
+        
+    
+  
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("sidebar-nav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+    
+    
+  }
+
+  
+
+
+
+
+//Announcement Box
+
+function announcementClose() {
+    var x = document.getElementById("announcement-wrapper");
+    
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+
+//news card section effects
+
+//news card 1
+
+//mouseOver
+function opacityFunction(){
+    document.getElementById("overlay1").style.opacity = '1';
+}
+
+function zIndexFuction(){
+    document.getElementById("news-card-body1").style.zIndex = '1';
+}
+
+/*function linkColorFunction(){
+    document.getElementById("news-card-body1").style.color = 'white';
+}*/
+
+
+//mouseOut
+
+function opacityFunctionR(){
+    document.getElementById("overlay1").style.opacity = '0';
+}
+
+function zIndexFuctionR(){
+    document.getElementById("news-card-body1").style.zIndex = '0';
+}
+
+/*function linkColorFunctionR(){
+    document.getElementById("news-card-body1").style.color = 'black';
+}*/
+
+//news card 2
+
+//mouseOver
+function opacityFunctionTwo(){
+    document.getElementById("overlay2").style.opacity = '1';
+}
+
+function zIndexFuctionTwo(){
+    document.getElementById("news-card-body2").style.zIndex = '1';
+}
+
+/*function linkColorFunctionTwo(){
+    document.getElementById("news-card-body2").style.color = 'white';
+}*/
+
+
+//mouseOut
+
+function opacityFunctionRTwo(){
+    document.getElementById("overlay2").style.opacity = '0';
+}
+
+function zIndexFuctionRTwo(){
+    document.getElementById("news-card-body2").style.zIndex = '0';
+}
+
+/*function linkColorFunctionRTwo(){
+    document.getElementById("news-card-body2").style.color = 'black';
+}*/
+
+//news card 3
+
+//mouseOver
+function opacityFunctionThree(){
+    document.getElementById("overlay3").style.opacity = '1';
+}
+
+function zIndexFuctionThree(){
+    document.getElementById("news-card-body3").style.zIndex = '1';
+}
+
+/*function linkColorFunctionThree(){
+    document.getElementById("news-card-body3").style.color = 'white';
+}*/
+
+
+//mouseOut
+
+function opacityFunctionRThree(){
+    document.getElementById("overlay3").style.opacity = '0';
+}
+
+function zIndexFuctionRThree(){
+    document.getElementById("news-card-body3").style.zIndex = '0';
+}
+
+/*function linkColorFunctionRThree(){
+    document.getElementById("news-card-body3").style.color = 'black';
+}*/
+
+
+
